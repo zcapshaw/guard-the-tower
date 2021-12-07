4178086a87a59583f887553118b5a59c3e8bb89a
Zach Capshaw <zcapshaw@gmail.com>
2021-12-06 19:12:57 -0700

    add epoch counter from etherscan api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,15 +22,21 @@ const IndexPage = () => {
   const [gpDisplayPrice, setGpDisplayPrice] = useState("-");
   const [ethPrice, setEthPrice] = useState(0);
   const [mintCost, setMintCost] = useState("-");
+  const [mintsRemaining, setMintsRemaining] = useState("-");
+  const [epoch, setEpoch] = useState(3);
+
+  const epochText = `Epoch ${epoch} Countdown`;
 
   // fetch price data from CoinGecko API
   useEffect(() => {
+    // for fun
+    console.log("!guard 🧙‍♂️🐉");
+
     // fetch ETH and $GP price
     fetch(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum%2C%20wizards-and-dragons&order=market_cap_desc`
     )
       .then((response) => {
-        console.log(response);
         if (response.ok) {
           return response.json();
         } else {
@@ -38,7 +44,6 @@ const IndexPage = () => {
         }
       })
       .then((resultData) => {
-        console.log(resultData);
         setEthPrice(resultData[0].current_price);
         setGpPrice(resultData[1].current_price);
         // error handling if the api fails to return
@@ -52,21 +57,48 @@ const IndexPage = () => {
 
     // set the cost to mint a wizard based on current ETH and GP prices
     setMintCost(calculateMintCost(ethPrice, gpPrice));
-    console.log(gpPrice, ethPrice);
-  }, [gpPrice, ethPrice]);
+    fetchTotalTokenCount();
+    fetchFloorData();
+  }, [gpPrice, ethPrice, floorPrice, epoch, mintsRemaining]);
 
-  // fetch opensea floor
-  useEffect(() => {
-    // fetch ETH and $GP price
+  const fetchFloorData = () => {
     fetch(
       `https://api.opensea.io/api/v1/collection/wizards-dragons-game-v2/stats`
     )
       .then((response) => response.json()) // parse JSON from request
       .then((resultData) => {
-        // console.log(resultData.stats.floor_price);
         setFloorPrice(resultData.stats.floor_price.toFixed(3));
       });
-  }, []);
+  };
+
+  const fetchTotalTokenCount = () => {
+    const apiKey = process.env.ETHERSCAN_API_KEY;
+
+    fetch(
+      `https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0x999e88075692bcee3dbc07e7e64cd32f39a1d3ab&apikey=${apiKey}`
+    )
+      .then((response) => response.json()) // parse JSON from request
+      .then((resultData) => {
+        // fetch the token count from etherscan
+        countdownCalculator(resultData.result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const countdownCalculator = (tokenCount) => {
+    if (tokenCount < 42000) {
+      setEpoch(3);
+      setMintsRemaining(42000 - tokenCount);
+    } else if (tokenCount < 51000) {
+      setEpoch(4);
+      setMintsRemaining(51000 - tokenCount);
+    } else if (tokenCount < 60000) {
+      setEpoch(5);
+      setMintsRemaining(60000 - tokenCount);
+    }
+  };
 
   const calculateMintCost = (ethPrice, gpPrice) => {
     const gpPricePerMint = 48000;
@@ -113,8 +145,8 @@ const IndexPage = () => {
         />
         <DataCard
           emoji="⏳"
-          title="Epoch 3 Countdown"
-          number="2031"
+          title={epochText}
+          number={mintsRemaining}
           footer="mints remaining"
         />
       </div>
