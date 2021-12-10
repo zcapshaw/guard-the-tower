5f10c127ff3cf599d94698cab5e892a4e9a709df
Zach Capshaw <zcapshaw@gmail.com>
2021-12-09 20:59:30 -0700

    fetch rrune date

--- src/components/item-card.tsx
+++ src/components/item-card.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ItemCard = ({ img, alt }) => {
+const ItemCard = ({ img, alt, price }) => {
   return (
     <div class="pb-4">
       <img src={img} alt={alt} class="w-20" />
-      <p class="text-xs md:text-base">0.09</p>
+      <p class="text-xs md:text-base">{price}</p>
     </div>
   );
 };
